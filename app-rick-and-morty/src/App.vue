6c72a5dea75ef228060c8e01bc2294430bd90f35
Gabriel Alves <gabriel52.alves@gmail.com>
2023-08-29 23:59:18 -0300

    footer

--- app-rick-and-morty/src/App.vue
+++ app-rick-and-morty/src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import HeaderDefault from './components/HeaderDefault.vue'
+import FooterDefault from './components/FooterDefault.vue';
 </script>
 
 <template>
   <HeaderDefault class="header"></HeaderDefault>  
-  <RouterView />
+
+  <RouterView class="content"/>
+
+  <FooterDefault class="footer"></FooterDefault>
 </template>
 
 <style>
+  .content {
+    height: 70%;
+  }
 </style>
---
+++ app-rick-and-morty/src/components/FooterDefault.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="footer" >
+        <p class="text-footer">
+            <img class="logo" src="../assets/images/github.svg" alt="github">
+            <a class="text-footer" href="https://github.com/gabriel04alves">
+                /gabriel04alves
+            </a>
+        </p>
+        <p class="text-footer">
+            <img class="logo" src="../assets/images/linkedin.svg" alt="linkedin">
+            <a class="text-footer" href="https://linkedin.com/in/gabriel04alves">
+                /gabriel04alves
+            </a>
+        </p>
+    </div>
+</template>
+
+<style>
+.footer {
+    display: flex;
+    justify-content: end;
+    /* align-items: center; */
+    flex-direction: row;
+    position: fixed;
+    gap: 1%;
+    border-top: 0.25em solid;
+    border-image: linear-gradient(60deg, #52b0cf, #97ce4c) 1 1;
+    color: #F4F4F4; 
+    height: 70px;
+    width: 100%; 
+    background-color: #002F40;
+    padding: 1%;
+}
+
+.text-footer {
+    font-family:'roboto', 'Times New Roman', Times, serif;;
+    text-decoration: none;
+    color: #f4f4f4;
+    font-weight: 300;
+    align-self: center;
+    display: flex;
+    justify-content: center;
+    gap: 2.5px;
+    font-size: 1.3vh;
+}
+
+.logo {
+    height: 2.6vh;
+    align-self: center;
+}
+@media (max-width: 1000px) {
+    .footer {
+        height: 50px;
+        justify-content: center;
+        gap: 15px;
+    }
+    .logo {
+        height: 3vh;
+    }
+    .text-footer {
+        margin-top: 0;
+        align-self: center;
+    }
+}
+</style>
+
+<script>
+export default {
+	name: "my-component",
+}
+</script>
