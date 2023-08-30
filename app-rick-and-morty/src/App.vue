b74c9b4bf7037e072084cd46cfdfe260fa156498
Gabriel Alves <gabriel52.alves@gmail.com>
2023-08-29 23:16:50 -0300

    init

---
+++ app-rick-and-morty/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import HeaderDefault from './components/HeaderDefault.vue'
+</script>
+
+<template>
+  <HeaderDefault class="header"></HeaderDefault>  
+  <RouterView />
+</template>
+
+<style>
+</style>
---
+++ app-rick-and-morty/src/components/HeaderDefault.vue
@@ -0,0 +1,28 @@
+<script>
+    import { RouterLink } from 'vue-router'
+    export default {
+        components: {
+            RouterLink
+        }
+    }
+</script>
+
+<template>
+    <div class="main">
+        <div>
+
+        </div>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+          </nav>
+    </div>
+</template>
+
+<style>
+    .main {
+        background-color: #002F40;
+        width: 100%;
+        height: 5rem;
+    }
+</style>
